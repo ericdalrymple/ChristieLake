8574bf866e59ca5ab900afba89702dcb15fee764
edalrymple <eric.dalrymple@gmail.com>
2021-05-02 16:44:47 -0400

    Results screen data!

--- Assets/Scripts/Game/UI/IResultsController.cs
+++ Assets/Scripts/Game/UI/IResultsController.cs
@@ -1,8 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public interface IResultsController : IUIController
 {
-    
+    int CurrentScore
+    {
+        get;
+    }
+
+    TimeSpan TimeElapsed
+    {
+        get;
+    }
 }
--- Assets/Scripts/Game/UI/ResultsView.cs
+++ Assets/Scripts/Game/UI/ResultsView.cs
@@ -1,8 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ResultsView : UIView<IResultsController>
 {
-    
+    [SerializeField]
+    private TMP_Text m_ScoreLabel;
+
+    [SerializeField]
+    private TMP_Text m_TimeLabel;
+
+    protected override void OnWillShow()
+    {
+        m_ScoreLabel.SetText(GetController().CurrentScore.ToString("N0"));
+
+        TimeSpan elapsed = GetController().TimeElapsed;
+        m_TimeLabel.SetText(
+            elapsed.Hours.ToString("D2") + ":" +
+            elapsed.Minutes.ToString("D2") + ":" +
+            elapsed.Seconds.ToString("D2") + "." +
+            (elapsed.Milliseconds / 10).ToString("D2"));
+    }
 }
