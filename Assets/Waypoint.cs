8f7cbd4869b529b4f5c7ca4d7b2f1bfca1abf313
edalrymple <eric.dalrymple@gmail.com>
2021-05-23 18:10:35 -0400

    Added a mechanism that scales waypoints according to how far they are to the player.

--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -1,7 +1,24 @@
+using System;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+    [Serializable]
+    protected struct WaypointScaling
+    {
+        public float MinScale;
+        public float MaxScale;
+
+        [Range(0.1f, 1000.0f)]
+        public float MaxDistance;
+    }
+
+    [SerializeField]
+    private Transform m_MeshTransform;
+
+    [SerializeField]
+    private WaypointScaling m_Scaling;
+
     private Animator m_BobAnimator;
 
     // Start is called before the first frame update
@@ -19,5 +36,20 @@ public class Waypoint : MonoBehaviour
     {
         Debug.Log("Waypoint reached!");
         transform.parent.BroadcastMessage(GameMessages.MSG_WAYPOINT_REACHED, null, SendMessageOptions.DontRequireReceiver);
+    }
+
+    void Update()
+    {
+        // Scale according to player distance
+        if (m_MeshTransform != null)
+        {
+            GameObject player = GameController.Instance.GetPlayer();
+            float playerDistance = Vector3.Distance(player.transform.position, transform.position);
+            playerDistance = Mathf.Min(m_Scaling.MaxDistance, playerDistance);
+
+            float normalizedPlayerDistance = playerDistance / m_Scaling.MaxDistance;
+            float scale = m_Scaling.MinScale + (normalizedPlayerDistance * (m_Scaling.MaxScale - m_Scaling.MinScale));
+            m_MeshTransform.localScale = new Vector3(scale, scale, scale);
+        }
     }
 }
